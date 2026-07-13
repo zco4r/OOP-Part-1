7ebd4aeb8fbd6d2cee9bb1e22f89191cc75d84c7
ammaralazki <alazkiyasser3@gmail.com>
2026-07-13 14:33:17 +0400

    calass for bankAccount

---
+++ OOP Part 1/Models/BankAccount.cs
@@ -0,0 +1,35 @@
+namespace OOP_Part_1.Models;
+
+public class BankAccount
+{
+    public int AccountNumber { get; set; }
+    public string HolderName { get; set; }
+    public double Balance { get; set; }
+
+    public void Deposit(double amount)
+    {
+       
+    }
+
+    public void Withdraw(double amount)
+    {
+        
+    }
+
+    public void CheckBalance()
+    {
+        
+    }
+
+    private void PrintInformation()
+    {
+        
+    }
+
+    private void SendEmail()
+    {
+        
+    }
+    
+    
+}
