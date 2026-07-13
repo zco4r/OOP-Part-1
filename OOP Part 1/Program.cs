8409d6e7d4471fb1d66084206895e53765310ccc
ammaralazki <alazkiyasser3@gmail.com>
2026-07-13 22:15:55 +0400

    added the cases

--- OOP Part 1/Program.cs
+++ OOP Part 1/Program.cs
@@ -27,6 +27,53 @@ class Program
             case "3" :
                 break;
             
+            case "4" :
+                break;
+            
+            case "5" :
+                break;
+            
+            case "6" :
+                break;
+            
+            case "7" :
+                break;
+            
+            case "8" :
+                break;
+            
+            case "9" :
+                break;
+            
+            case "10" :
+                break;
+            
+            case "11" :
+                break;
+            
+            case "12" :
+                break;
+            
+            case "13" :
+                break;
+            
+            case "14" :
+                break;
+            
+            case "15" :
+                break;
+            
+            case "16" :
+                break;
+                
+            case "17" :
+                break;
+            
+            case "18" :
+                break;
+            
+            case "19" :
+                break;
             default:
                 Console.WriteLine("Invalid selection. Please restart and choose 1 or 2.");
                 break;
