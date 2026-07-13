aac9c31709e51a2919a524f43a0bfe4fcd11ef31
ammaralazki <alazkiyasser3@gmail.com>
2026-07-13 21:57:27 +0400

    +

--- OOP Part 1/Models/Product.cs
+++ OOP Part 1/Models/Product.cs
@@ -6,10 +6,19 @@ public class Product
     public string ProductName  { get; set; }
     public double Price  { get; set; }
     public int StockQuantity   { get; set; }
-
+    
+    
     public void Sell(int quantity)
     {
-        
+        if (StockQuantity >= quantity)
+        {
+            StockQuantity -= quantity;
+        }
+        else
+        {
+            Console.WriteLine("Sorry, the available stock quantity is insufficient.");
+        }
+        LogTransaction();
     }
 
     public void Restock(int quantity)
