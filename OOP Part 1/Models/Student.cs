9fa0e75ad886663252c1a05a6fb5a2197dc10d61
ammaralazki <alazkiyasser3@gmail.com>
2026-07-13 14:34:35 +0400

    class for student

---
+++ OOP Part 1/Models/Student.cs
@@ -0,0 +1,21 @@
+namespace OOP_Part_1.Models;
+
+public class Student
+{
+    public int Gread { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+    private string email { get; set; }
+    int age { get; set; }
+
+    public void Register(string Email)
+    {
+        
+    }
+
+    public void SendEmail()
+    {
+        
+    }
+    
+}
